6a319456ae1386ef7476751baed0cc165f0cb52f
bensonben654 <10060119Bb>
2020-02-09 10:46:02 -0500

    added master list, stub DB and started adding SQLalchemy to talk to the DB

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+# with open('Db-01.json') as f:
+#     datas = json.load(f)
+
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///StubServersDB.db'
+db = SQLAlchemy(app)
+
+db.Model.metadata.reflect(db.engine)
+
+
+class Server(db.Model):
+    __tablename__ = 'Server'
+    __table_args__ = {'extend_existing': True}
+    ServerId = db.Column(db.Text, primary_key=True)
+
 
 # 5:30 of video 2 importing data (JSON)
 ''' 
@@ -14,7 +33,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
+    print("Total number of servers is: ", Server.query.count())
     return render_template('HomePage.html')
+
+
+@app.route('/masterlist')
+def master_list():
+    return render_template('MasterList.html')
 
 
 @app.route('/real-time-data-overview')
@@ -25,6 +50,11 @@ def RTDO():
 @app.route('/usage-cpu')
 def usage_CPU():
     return render_template('Usage-CPU.html')
+
+
+# @app.route('/' + str(datas.ServerName).lower() + '-rt')
+# def RTDO():
+#     return render_template('RealTimeDataOverview.html', datas=datas)
 
 
 if __name__ == '__main__':
