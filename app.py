b6e0680312c179a7ac48309ca73179f82658d887
bensonben654 <912051599Bb>
2020-02-12 14:31:04 -0500

    Real time page no longer hardcoded. -Ben

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class Metric(db.Model):
     __tablename__ = 'Metric'
     __table_args__ = {'extend_existing': True}
     MetricId = db.Column(db.Text, primary_key=True)
-    ServerID = db.Column(db.Text, db.ForeignKey('Server.ServerId'))  # n,ullable=False)
+    ServerID = db.Column(db.Text, db.ForeignKey('server.ServerId'))  # ,nullable=False)
 
 
 # 5:30 of video 2 importing data (JSON)
@@ -39,7 +39,7 @@ class Metric(db.Model):
 @app.route('/home')
 def home():
     server_table = Server.query.all()
-    print("Total number of servers is: ", Server.query.count())
+    # print("Total number of servers is: ", Server.query.count())
     return render_template('HomePage.html', server=server_table)
 
 
@@ -55,8 +55,21 @@ def RTDO():
 
 @app.route('/real-time-data-overview/<slug>')
 def RT(slug):
+    # print(Server.query.join(Metric, Metric.ServerID == Server.ServerId).Cpu)
+    # records = session.query(Customer).join(Order, Order.customer_id == Customer.id).all()
+    # metric = Metric.query.filter(Metric.ServerID == Server.ServerId)
+
     server = Server.query.filter_by(ServerId=slug).first()
-    return render_template('RealTimeDataOverviewTemp.html', server=server)
+    print(slug)
+    # peter = User.query.filter_by(username='peter').first()
+    # peter.id
+    tmp = Metric.query.filter_by(ServerID=slug).scalar()
+    print(tmp.MetricId)
+    metric_table = Metric.query.get(
+        tmp.MetricId)  # Metric.query.filter(Metric.ServerID == Server.ServerId).max(Metric.Time)
+    # cpu = Metric.Cpu.query.filter_by(ServerID=slug)
+
+    return render_template('RealTimeDataOverviewTemp.html', server=server, metric=metric_table)
 
 
 @app.route('/usage-cpu')
